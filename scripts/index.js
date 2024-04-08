66cac4e42689e33d4d43e320b07a656a6fb48428
splatonov <sergey.platonov.official@gmail.com>
2024-04-08 09:45:34 +0300

    Изменены названия переменных

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,27 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(cardName, cardLink, delCardOnClick) {
+function addCard(cardName, cardLink, deleteCardOnClick) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const delButton = cardElement.querySelector('.card__delete-button');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
 
   cardImage.setAttribute('src', cardLink);
   cardImage.setAttribute('alt', cardName);
   cardTitle.textContent = cardName;
-  delButton.addEventListener('click', () => {
-    delCardOnClick(cardElement);
+  cardDeleteButton.addEventListener('click', () => {
+    deleteCardOnClick(cardElement);
   })
   
   return cardElement
 }
 
 // @todo: Функция удаления карточки
-function delCard(CardItem) {
+function deleteCard(CardItem) {
   CardItem.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-  placesList.append(addCard(item.name, item.link, delCard));
+  placesList.append(addCard(item.name, item.link, deleteCard));
 })

a0ee4b311f1799ceed6d6628590f4270ce83cb78
splatonov <sergey.platonov.official@gmail.com>
2024-04-08 10:14:08 +0300

    Доработана функция добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -4,15 +4,15 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(cardName, cardLink, deleteCardOnClick) {
+function addCard(cardData, deleteCardOnClick) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
 
-  cardImage.setAttribute('src', cardLink);
-  cardImage.setAttribute('alt', cardName);
-  cardTitle.textContent = cardName;
+  cardImage.setAttribute('src', cardData.link);
+  cardImage.setAttribute('alt', cardData.name);
+  cardTitle.textContent = cardData.name;
   cardDeleteButton.addEventListener('click', () => {
     deleteCardOnClick(cardElement);
   })
@@ -26,5 +26,5 @@ function deleteCard(CardItem) {
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-  placesList.append(addCard(item.name, item.link, deleteCard));
+  placesList.append(addCard(item, deleteCard));
 })
